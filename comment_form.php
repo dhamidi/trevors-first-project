e9393e44cf1bc5aade0b6295144fd53e10973009
Dario Hamidi <dario@gowriteco.de>
2016-01-31 13:29:04 +0200

    Use correct comment syntax for PHP
    Comments in PHP are introduced with /* and ended with */.  Only in HTML
    comments are introduced with <!-- and ended with -->.  Using HTML
    comments in PHP led to syntax errors and the script did not run.

--- comment_form.php
+++ comment_form.php
@@ -1,5 +1,5 @@
 <?php
-define ('DB_NAME', 'comments') ; 
+define ('DB_NAME', 'comments') ;
 define ('DB_USER', 'root' ) ;
 define ('DB_PASSWORD', '' ) ;
 define ('DB_HOST', 'localhost' ) ;
@@ -9,7 +9,7 @@ $link = mysql_connect (DB_HOST, DB_USER, DB_PASSWORD);
 
 if (!$link)  {
 
-  die ('could not connect : ' . mysql_error ()) ; 
+  die ('could not connect : ' . mysql_error ()) ;
 }
 
   $database_selected = mysql_select_db ('DB_NAME', $link) ;
@@ -20,28 +20,27 @@ if (!$link)  {
   die('Could not connect: ' . mysql_error());
 }
 
-echo 'Connected successfully database'; 
+echo 'Connected successfully database';
 
- 
+
   $name = $_POST ["name"];
   $message = $_POST ["comment"];
   $email = $_POST ["email"];
-  
-  <!--- Latest work! All seems fine no error messages. Open to improvements, comments and suggestions--->
+
+  /* Latest work! All seems fine no error messages. Open to improvements, comments and suggestions */
 
   $sql = "INSERT INTO comments.commentform (`name`, `email`, `comment`) VALUES ('$name','$email', '$message');";
-  
+
   echo "$name","<br>", "$message";
 
-  
+
 if (!mysql_query($sql)) {
 
   die('Error: ' . mysql_error());
 }
 
-  
-  
-  
+
+
+
 mysql_close()
 ?>
-
